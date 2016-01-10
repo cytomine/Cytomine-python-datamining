a412df8bfc8f5c5a71a2b937e76924883d198264
Waliens <romainmormont@hotmail.com>
2016-01-10 17:24:33 +0100

    re-implemente aggregate_segmenter

--- cytomine-datamining/algorithms/sldc/aggregatesegmenter.py
+++ cytomine-datamining/algorithms/sldc/aggregatesegmenter.py
@@ -13,17 +13,13 @@ __author__ = "Begon Jean-Michel <jm.begon@gmail.com>"
 __copyright__ = "Copyright 2010-2013 University of Liège, Belgium"
 __version__ = '0.1'
 
-import numpy as np
 import cv2
-
-import cv
 import copy
+import numpy as np
 from scipy.ndimage.filters import maximum_filter
 from scipy.ndimage.measurements import label
 from helpers.datamining.segmenter import BinarySegmenter
-from helpers.utilities.datatype.imageconverter import NumpyConverter
 from helpers.datamining.segmenter import otsu_threshold_with_mask
-
 
 class AggregateSegmenter(BinarySegmenter):
     """
@@ -33,17 +29,18 @@ class AggregateSegmenter(BinarySegmenter):
     A :class:`Segmenter` for segmenting cells in aggregate and architectural
     pattern
     """
-
-    def __init__(self, color_deconvoluter, struct_elem,
-                 cell_max_area,
-                 cell_min_circularity=.8,
-                 border=7,
-                 ):
+    def __init__(self, color_deconvoluter, struct_elem, cell_max_area=4000, cell_min_circularity=.8):
         self._color_deconvoluter = color_deconvoluter
         self._struct_elem = struct_elem
         self._cell_max_area = cell_max_area
         self._min_circularity = cell_min_circularity
-        self._border = border
+        self._small_struct_element = np.array([
+            [0, 1, 1, 1, 0],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [0, 1, 1, 1, 0]
+        ]).astype(np.uint8)
 
     def segment(self, np_image):
         """
@@ -52,101 +49,92 @@ class AggregateSegmenter(BinarySegmenter):
         np_image : numpy.ndarray
             A RGBA image to segment
         """
-        border = self._border
-        alpha = np.array(np_image[:, :, 3])
-        image = np.array(np_image[:, :, 0:3])
-        temp = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        im_dec = self._color_deconvoluter.transform(temp)
-        temp = cv2.cvtColor(im_dec, cv2.COLOR_RGB2GRAY)
-        otsu_threshold, internal_binary = otsu_threshold_with_mask(temp, alpha, cv2.THRESH_BINARY_INV)
+        # Extract alpha mask and RGB, cast to uint8 to match opencv types
+        alpha = np.array(np_image[:, :, 3]).astype(np.uint8)
+        image = np.array(np_image[:, :, 0:3]).astype(np.uint8)
+
+        # Perform color conversions and deconvolution
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_dec = self._color_deconvoluter.transform(image_rgb)
+        image_grey = cv2.cvtColor(image_dec, cv2.COLOR_RGB2GRAY)
+
+        # Find and apply Otsu threshold
+        otsu_threshold, internal_binary = otsu_threshold_with_mask(image_grey, alpha, cv2.THRESH_BINARY_INV)
         internal_binary_copy = copy.copy(internal_binary)
+
+        # Find interior contours (possibly inclusion that were missed because of their color) and remove the
+        # corresponding artifacts
         contours2, hierarchy = cv2.findContours(internal_binary_copy, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-        #filling inclusions without filling inter-cell space
+        # Remove contours that don't have a parent contour (???)
+        #   contours2 is somtimes so big that calling enumerate on it causes freezes
+        #   this pre-filtering to prevent from enumerating this big object
+        contour_idx = [i for i, h in enumerate(hierarchy[0]) if h[3] >= 0]
+
         mask = np.zeros(internal_binary.shape)
 
-        for i, contour in enumerate(contours2):
+        for i in contour_idx:
+            contour = contours2[i] # fetch the contour to evaluate
 
-            if (hierarchy[0, i, 3] != -1): #internal contour
+            # Filter contour to make sure it should be filled, get convex hull of the contour to avoid artifact
+            convex_hull = cv2.convexHull(contour)
+            convex_area = cv2.contourArea(convex_hull)
+            perimeter = cv2.arcLength(convex_hull,True)
+            circularity = 4 * np.pi * convex_area / (perimeter * perimeter)
 
-                convex_hull = cv2.convexHull(contour)
+            # Remove small objects (first cleaning)
+            if convex_area < (self._cell_max_area / 10):
+                cv2.drawContours(internal_binary, [convex_hull], -1, 255, -1)
 
-                convex_area = cv2.contourArea(convex_hull)
+            # Remove potential inclusions
+            if (convex_area < self._cell_max_area/2) and (circularity > self._min_circularity):
+                cv2.drawContours(internal_binary, [convex_hull], -1, 255, -1)
 
-                perimeter = cv2.arcLength(convex_hull,True)
-
-                circularity = 4*np.pi*convex_area / (perimeter*perimeter)
-
-                #visualisation
-                #cv2.drawContours(np_image, contour, -1, (0,255,255), -1)
-
-                if (convex_area < self._cell_max_area/10):
-
-                    #removing small objects
-
-                    cv2.drawContours(internal_binary, [convex_hull], -1, 255, -1)
-
-                if (convex_area < self._cell_max_area/2) and (circularity > self._min_cirulatiry):
-
-                    #removing potential inclusions
-
-                    cv2.drawContours(internal_binary, [convex_hull], -1, 255, -1)
-
+        # Deletion of small holes
         internal_binary = cv2.morphologyEx(internal_binary,cv2.MORPH_CLOSE, self._struct_elem, iterations = 1)
-
         internal_binary = cv2.morphologyEx(internal_binary,cv2.MORPH_OPEN, self._struct_elem, iterations = 2)
 
+        # Watershed in order to separate neighbouring arrays
+        #  1) Find the markers for starting the watershed (using distance transform)
+        #    -> find local maximum of the distance transform
+        #    -> create a mask for the markers : (255) for the markers, and (0) for the rest
+        #  2)
+        # TODO test dt on dilated internal_binary
         dt = cv2.distanceTransform(internal_binary, cv2.cv.CV_DIST_L2, 3)
 
-        dt = dt[0]
+        #image_dec[internal_binary == 0] = [255,255,255] # (????)
 
-        im_dec[internal_binary == 0] = (255,255,255)
+        # Create borders to be added to the markers' image
+        borders = cv2.dilate(internal_binary, self._struct_elem, iterations=1)
+        borders = borders - cv2.erode(borders, None)
 
-        #detection maxima locaux
-        local_min_ind = maximum_filter(dt, (9,9) ) == dt
+        # Detection maxima locaux
+        local_max_ind = maximum_filter(dt, size=9) == dt
 
-        #image markers
+        # Create marker mask
         markers = np.zeros(dt.shape).astype(np.uint8)
-
-        #maxima locaux
-        markers[local_min_ind] = 255
-
-        #impose tous les markers sont a l'interieur du contour
+        markers[local_max_ind] = 255
         markers[internal_binary == 0] = 0
 
-        markers = cv2.dilate(markers,self._struct_elem, iterations = 2)
-
+        # Dilate marker to make them more homogeneous
+        markers = cv2.dilate(markers, self._small_struct_element, iterations = 2)
         markers = markers.astype(np.int32)
 
-        #labellise les composantes connexes 1...nbmarkers
+        # Differentiates the colors of the markers (labelling)
         markers, nb_labels = label(markers, np.ones((3,3)))
+        # Use 255 as color for borders to distinguish pixels from the background
+        if nb_labels < 255:
+            markers[markers == 255] = 256
+            markers[borders == 255] = 255
+        else:
+            markers[markers == 255] = nb_labels + 1
+            markers[borders == 255] = 255
 
-        borders = cv2.dilate(internal_binary, self._struct_elem, iterations=1)
+        # Execute watershed
+        cv2.watershed(image_dec, markers)
 
-        markers[borders == 0] = 0
-
-        borders = borders - internal_binary
-
-        #cadre blanc autour (pour eviter contour de la taille iamge?)
-        markers[borders == 255] = nb_labels+2
-
-        markers = cv2.copyMakeBorder(markers, border, border, border, border, cv2.BORDER_CONSTANT, value = nb_labels+2)
-
-        im_dec = cv2.copyMakeBorder(im_dec, border, border, border, border, cv2.BORDER_CONSTANT, value = (255,255,255))
-
-        cv2.watershed(im_dec, markers)
-
-        #enleve cadre
-        markers = markers[border:-border, border:-border]
-
-        #repasse en opencv (pour compatibilité objectfinder)
-        internal_binary = np.zeros(internal_binary.shape).astype(np.uint8)
-
-        cv_image = cv.CreateImageHeader((internal_binary.shape[1], internal_binary.shape[0]), cv.IPL_DEPTH_8U, 1)
-        cv.SetData(cv_image, mask.tostring())
-        return NumpyConverter().convert((cv_image))
-
-
-
-
-
+        # Remove barriers produced by watershed
+        markers[markers == -1] = 0
+        markers[markers == 255] = 0
+        markers[markers > 0] = 255
+        return markers.astype(np.uint8)
