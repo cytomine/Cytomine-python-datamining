53ba19e3e7fe60742288b28a80f4d25b54703954
Romain Mormont <r.mormont@student.ulg.ac.be>
2016-03-05 01:30:36 +0100

    add chaining to init

--- cytomine-datamining/algorithms/thyroid/sldc/__init__.py
+++ cytomine-datamining/algorithms/thyroid/sldc/__init__.py
@@ -7,9 +7,10 @@ from .dispatcher import DispatchingRule, DispatcherClassifier
 from .workflow import SLDCWorkflow
 from .classifier import PolygonClassifier
 from .errors import ImageExtractionError, TileExtractionError
+from .chaining import ImageProvider, WorkflowLinker, WorkflowChain
 
 __author__ = "Romain Mormont <r.mormont@student.ulg.ac.be>"
 
 __all__ = ["Locator", "Segmenter", "DispatcherClassifier", "DispatchingRule", "SLDCWorkflow", "Image", "Tile",
            "TileBuilder", "PolygonClassifier", "TileTopology", "TileTopologyIterator", "ImageExtractionError",
-           "TileExtractionError", "ImageWindow"]
+           "TileExtractionError", "ImageWindow", "ImageProvider", "WorkflowLinker", "WorkflowChain"]
