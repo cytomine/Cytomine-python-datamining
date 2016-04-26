3515e4153c9dddfe31ca3199eb26b60974b9c74c
Romain Mormont <r.mormont@student.ulg.ac.be>
2016-04-26 11:40:36 +0200

    move common functions to util.py

--- cytomine-applications/util/custom_validation/build.py
+++ cytomine-applications/util/custom_validation/build.py
@@ -2,10 +2,8 @@
 import optparse
 import os
 
-import shutil
-
+from util import CropTypeEnum, copy_content, create_dir, str2bool, str2list
 from cytomine import cytomine
-from cytomine.models import Annotation
 from pyxit import pyxitstandalone
 
 from mapper import BinaryMapper, DefaultMapper
@@ -13,46 +11,6 @@ from cytominejob import CytomineJob
 
 __author__ = "Mormont Romain <romain.mormont@gmail.com>"
 __version__ = "0.1"
-
-
-def str2bool(v):
-    return v.lower() in ("yes", "true", "t", "1")
-
-
-def str2list(l, conv=int):
-    print "PRINT: {}".format(l)
-    return [conv(v) for v in l.split(",")]
-
-
-def create_dir(path, clean=False):
-    if not os.path.exists(path):
-        print "Creating annotation directory: %s" % path
-        os.makedirs(path)
-    elif clean:
-        print "Cleaning annotation directory: %s" % path
-        shutil.rmtree(path)
-        os.makedirs(path)
-
-
-def copy_content(src, dst):
-    files = [f for f in os.listdir(src) if not os.path.isdir(os.path.join(src, f))]
-    for f in files:
-        shutil.copy(os.path.join(src, f), dst)
-
-
-class CropTypeEnum(object):
-    CROP = 1
-    ALPHA_CROP = 2
-
-    @staticmethod
-    def enum2crop(enum):
-        if enum == CropTypeEnum.CROP:
-            return Annotation.get_annotation_crop_url
-        elif enum == CropTypeEnum.ALPHA_CROP:
-            return Annotation.get_annotation_alpha_crop_url
-        else:
-            raise ValueError("Invalid enum field : {}".format(enum))
-
 
 class ModelBuilderJob(CytomineJob):
 
---
+++ cytomine-applications/util/custom_validation/util.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Mormont Romain <romain.mormont@gmail.com>"
+__version__ = "0.1"
+
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+
+def str2list(l, conv=int):
+    return [conv(v) for v in l.split(",")]
+
+
+def create_dir(path, clean=False):
+    if not os.path.exists(path):
+        print "Creating annotation directory: %s" % path
+        os.makedirs(path)
+    elif clean:
+        print "Cleaning annotation directory: %s" % path
+        shutil.rmtree(path)
+        os.makedirs(path)
+
+
+def copy_content(src, dst):
+    files = [f for f in os.listdir(src) if not os.path.isdir(os.path.join(src, f))]
+    for f in files:
+        shutil.copy(os.path.join(src, f), dst)
+
+
+class CropTypeEnum(object):
+    CROP = 1
+    ALPHA_CROP = 2
+
+    @staticmethod
+    def enum2crop(enum):
+        if enum == CropTypeEnum.CROP:
+            return Annotation.get_annotation_crop_url
+        elif enum == CropTypeEnum.ALPHA_CROP:
+            return Annotation.get_annotation_alpha_crop_url
+        else:
+            raise ValueError("Invalid enum field : {}".format(enum))
+
