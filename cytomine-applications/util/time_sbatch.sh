017f75f78fa750b8cf0f1ef803cf04fb0b4fe612
Romain Mormont <r.mormont@student.ulg.ac.be>
2016-04-29 23:16:05 +0200

    update sldc

---
+++ cytomine-applications/util/time_sbatch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=exec_time
+#SBATCH --output=/home/mass/GRD/r.mormont/out/test/time.res
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=96:00:00
+#SBATCH --mem=1G
+#SBATCH --partition=Public
+#time wget http://www.montefiore.ulg.ac.be/~maree/cytomine/Tumor_002.tif
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
+time wget http://beta.cytomine.be/api/imageinstance/8122868/window-0-0-2048-2048.png
