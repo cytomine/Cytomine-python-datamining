540335683861efb5017f3730b34461868c3112d2
Romain Mormont <romainmormont@hotmail.com>
2017-02-10 11:00:55 +0100

    add bash for running example

---
+++ cytomine-datamining/algorithms/sldc/examples/with_pyxit/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python add_and_run_job.py \
+    --cytomine_id_software 19718236 \
+    --cytomine_id_project 526946 \
+    --cytomine_id_image 527252 \
+    --sldc_tile_overlap 7 \
+    --sldc_tile_width 768 \
+    --sldc_tile_height 768 \
+    --pyxit_model_path "model.pkl" \
+    --n_jobs 4 \
+    --min_area 500 \
+    --threshold 215 \
+    --rseed 0 \
+    --working_path "/tmp/sldc"
